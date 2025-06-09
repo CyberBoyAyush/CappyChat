83aa1ff38a9bf500daf3f5e4ef446b62ed22c6c5
Ayush Sharma <aayush240204@gmail.com>
2025-06-09 22:00:00 +0530

    Initial push

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './styles.css';
+import 'katex/dist/katex.min.css';
+import { Toaster } from '@/frontend/components/ui/sonner';
+import { ThemeProvider } from '@/frontend/components/ui/AppThemeProvider';
+import { Analytics } from '@vercel/analytics/react';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'ATChat',
+  description: 'Fastest AI Chat App',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster richColors position="top-right" />
+        </ThemeProvider>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
